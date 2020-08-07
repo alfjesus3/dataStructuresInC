f3b39fb98f767815471f471d6d32fba5e439fea4
André Jesus <alfjesus3@gmail.com>
2020-08-07 11:17:31 +0200

    Minor changes

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # dataStructuresInC
 - This project contains my implementations of relevant Data structures at low level abstraction using C.
 
+
+Usage
+-------
+
+```
+// First the testFile and the dataStructure are compiled separatly
+gcc -Wall -c <testFile>.c
+gcc -Wall -c <dataStructure>.c
+// The object files generated previously are linked together to make the final executable file
+gcc -o testing <dataStructure>.o <testFile>.o
+
+```
--- testLinkedList.c
+++ testLinkedList.c
@@ -4,16 +4,23 @@
 
 #define MAX_C 10
 
-// Issues faced:
-// When creating a pointer inside main, there is a temporary allocation on memory
-// The other solution is to use malloc but then it is not possible to distinguish between
-// garbage and actual data
-    // In this situation use global variables
 
 // In this file, several tests are performed to guarantee the 
 // correctness of the linked list implementation
 
 struct node *start = NULL;// global variable
+
+void transverseListAndPrint(struct node **start)
+{
+    //Transversing the list 
+    struct node *ptr, *nPtr;
+    ptr = *start;
+    
+    while((nPtr = (*ptr).next) != NULL){
+        printf("%d\n", (*nPtr).data);
+        ptr = nPtr;
+    }
+}
 
 int main (void)
 {
@@ -28,13 +35,8 @@ int main (void)
     for(i =0; i< MAX_C; i++){
         insertNodeEnd(&start, i);
     }
-    
-    struct node *ptr, *nPtr;
-    ptr = start;
-    
-    while((nPtr = (*ptr).next) != NULL){
-        printf("%d\n", (*nPtr).data);
-        ptr = nPtr;
-    }
+   
+    transverseListAndPrint(&start);
+
     return 0;
 }
